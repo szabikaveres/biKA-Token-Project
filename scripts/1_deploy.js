b3194c3cd77fd03685a5c0d1c24ed0eaeb99c991
Szabolcs Veres <szabolcsveres2022@gmail.com>
2023-12-11 19:53:41 +0000

    smoke test

---
+++ scripts/1_deploy.js
@@ -0,0 +1,17 @@
+async function main() {
+         //Fetch contract to deploy
+         const Token = await ethers.getContractFactory("Token") //gets the info from Token.json
+
+        //Deploy contract
+        const token = await Token.deploy() // sending it away to BC
+        await token.deployed() //fetch the copy of the token which has been deployed
+        console.log(`Token Deployed to: ${token.address}`) //formatting -putting it inside of a string
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
+
